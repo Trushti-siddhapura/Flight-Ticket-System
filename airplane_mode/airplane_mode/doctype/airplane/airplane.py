62ac1c4f37d2e331bf05dd4151dc49b388adad94
Sanskar <trushti@sanskar.com>
2025-03-04 19:04:14 +0530

    "Added the mutiple Doctypes Here

---
+++ airplane_mode/airplane_mode/doctype/airplane/airplane.py
@@ -0,0 +1,15 @@
+import frappe
+from frappe.model.document import Document
+
+class Airplane(Document):
+    def autoname(self):
+     
+        airline = self.airline.replace(" ", "")
+
+  
+        total_airplane = frappe.db.count("Airplane", {"airline": self.airline}) + 1
+
+ 
+        airline_number = str(total_airplane).zfill(3)
+
+        self.name = f"{airline}-{airline_number}"
