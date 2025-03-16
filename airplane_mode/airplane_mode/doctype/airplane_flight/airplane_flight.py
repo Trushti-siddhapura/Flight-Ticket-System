e8918373713a172c1181b90647a4ddabb0217d63
Trushti-Siddhapura <siddhapuratrushti2211@gmail.com>
2025-03-16 18:30:04 +0530

    Changes

--- airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
+++ airplane_mode/airplane_mode/doctype/airplane_flight/airplane_flight.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, Trushti-Siddhapura and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 from frappe.model.document import Document
 
 class AirplaneFlight(WebsiteGenerator):	
 	pass
+
+
+def get_context(context):
+	doc = frappe.get_all("Airplane Flight")
+	context.duration = frappe.utils.format_duartion(doc.duration)
